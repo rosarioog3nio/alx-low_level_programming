9a5643f0e3c3af3a15ceb177bf5ed67ea46d4e00
rosario0g3nio <rosarioogenio@gmail.com>
2022-12-08 10:21:50 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-int main(){
-    char str1[100];
+int main()
+{
+char str1[52];
 
-    strcpy(str1, "Programming is like building a multilingual puzzle \n");
-    puts(str1);
-    return (0);
+strcpy(str1, "Programming is like building a multilingual puzzle\n");
+puts(str1);
+return (0);
 }
