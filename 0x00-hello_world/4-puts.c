8d7fd61b0ef75041dec0706a37d35c058d06a4e4
rosario0g3nio <rosarioogenio@gmail.com>
2022-12-08 13:54:47 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <string.h>
+/*
+* main - print a simple message using puts
+*
+*Description: print the "Programming is like building a multilingual puzzle using puts
+*/
+
 int main(void)
 {
-char str1[52];
+	char str1[52];
 
-strcpy(str1, "Programming is like building a multilingual puzzle\n");
-puts(str1);
-return (0);
+	strcpy(str1, "Programming is like building a multilingual puzzle\n");
+	puts(str1);
+	return (0);
 }
