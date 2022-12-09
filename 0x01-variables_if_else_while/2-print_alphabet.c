565bea7b1755dbf7be32b35c13b1c9f2c019bfbd
rosario0g3nio <rosarioogenio@gmail.com>
2022-12-09 04:14:00 -0800

    alphabet in lowecase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Alphabet game
+*
+*Description: A program that print the alphabet in lowercase
+*
+*Return: Always 0 (Success)_
+*/
+
+int main(void)
+{
+	char chr;
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		putchar(chr);
+	}
+	return(0);
+}
