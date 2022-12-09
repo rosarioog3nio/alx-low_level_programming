887815230180fe6eb9443a3fc5f6df3ae0bb71e8
rosario0g3nio <rosarioogenio@gmail.com>
2022-12-09 03:39:49 -0800

    assign a random number to the variable n each time it is executed and print it's last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,20 +7,21 @@
 int main(void)
 {
 	int n;
-
+	int lastDigit;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	printf("Last digit of %d is ", n);
-	if (n % 10 > 5)
+	lastDigit = n % 10;
+	printf("Last digit of %d is ", lastDigit);
+	if (lastDigit > 5)
 	{
 		printf("and is greater that 5\n");
 	}
-	else if (n % 10 == 0)
+	else if (lastDigit == 0)
 	{
 		printf("and is 0\n");
 	}
-	else if (n % 10 < 6 && n % 10 != 0)
+	else if (lastDigit < 6 && lastDigit % 10 != 0)
 	{
 		printf("and is less than 6 and not 0\n");
 	}
