35bd17c9d25d033d7d4f22de15714827a9888dac
rosario0g3nio <rosarioogenio@gmail.com>
2022-12-09 17:02:57 -0800

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+*main - base 16 numbers 
+*
+*Description: printbase 16 number
+*
+*Return: Success (0)
+*/
+\
+int main()
+{
+	int num = '0'; /*Initialize num to 0*/
+	char letter = 'a'; /*Initialize letter to a*/
+
+	while (num <= '9') /*Print 0-9*/
+	{
+		putchar(num);
+		num++;
+	}
+	while (letter <= 'f') /*Print a-f*/
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
