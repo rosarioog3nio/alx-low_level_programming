1a931d876458d11dfc5d3dd6233c427ca8482293
rosario0g3nio <rosarioogenio@gmail.com>
2022-12-13 03:27:52 -0800

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/***
+*main - print putchar_
+*
+*Description - Program that print putchar_
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	printf("putchar_\n");
+	return (0);
+}
