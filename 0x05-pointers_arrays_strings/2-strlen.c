4025a2356fbee0ad1f4f8698841d2518e95f17f5
rosario0g3nio <rosarioogenio@gmail.com>
2022-12-19 14:09:48 -0800

    return lenth of a str

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+*int_strlen - retun the length of a scring
+*
+*@s: the string pointer
+*
+*Return: nothing
+*/
+
+int_strlen(char *s)
+{
+	strlen(*s);
+}
