f817f5ab1c296324312d758beaefc52a1849f760
rosario0g3nio <rosarioogenio@gmail.com>
2022-12-19 14:46:10 -0800

    print  a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
-*void_puts - print a string
+*_puts - print a string
 *
 *@str: the string to print
 *
@@ -13,7 +13,7 @@ void _puts(char *str)
 {
 	int i = 0;
 
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
